a91244496299345a0c3c1ac46f70d67573a66fd0
Jose Nogueira <jose.nogueira@eyenov.com>
2021-11-11 12:03:04 +0000

    Add `Package.swift`

---
+++ LiveryTargetSource/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jose Nogueira on 27/10/2021.
+//
+
+import Foundation
---
+++ Package.swift
@@ -0,0 +1,51 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+// Current stable version of the Livery iOS SDK
+let latestVersion = "1.3.0-beta.1"
+
+// Hosting url where the release artifacts are hosted.
+let hostingUrl = "https://sdk-ios-binaries.liveryvideo.com/xcframework/"
+let localPath = "XCF/"
+let localPathEnabled = false
+
+func createTarget() -> Target {
+    localPathEnabled ?
+    Target.binaryTarget(name: "LiveryFramework",
+                        path: "\(localPath)Livery.xcframework") :
+    Target.binaryTarget(name: "Livery",
+                        url: "\(hostingUrl)Livery-\(latestVersion).zip",
+                        checksum: "79d6dc6dcc6bd784e198f7526e4584d47ddc926ffcaf07d4b2b31d009fcd55c8")
+}
+
+let target = createTarget()
+
+let package = Package(
+    name: "Livery",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "Livery", targets: ["LiverySDKWrapper"])
+    ],
+    dependencies: [
+        .package(
+            name: "AWSiOSSDKV2",
+            url: "https://github.com/aws-amplify/aws-sdk-ios-spm",
+            from: "2.26.0"
+        )
+    ],
+    targets: [
+        target,
+        .target(
+            name: "LiverySDKWrapper",
+            dependencies: [
+                "Livery",
+                .product(name: "AWSCore", package: "AWSiOSSDKV2"),
+                .product(name: "AWSPinpoint", package: "AWSiOSSDKV2")
+            ],
+            path: "LiveryTargetSource"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)

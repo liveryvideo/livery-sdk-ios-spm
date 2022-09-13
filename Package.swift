7b47a848fb991b3e2c67243cbc84019cc288fab8
CircleCI <distiller@static.38.39.178.62.cyberlynk.net>
2022-09-13 10:05:43 +0000

    Livery (2.0.0-alpha.1)

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 // Current stable version of the Livery iOS SDK
-let latestVersion = "1.4.1"
+let latestVersion = "2.0.0-alpha.1"
 
 // Hosting url where the release artifacts are hosted.
 let hostingUrl = "https://sdk-ios-binaries.liveryvideo.com/xcframework/"
@@ -15,7 +15,7 @@ func createTarget() -> Target {
                         path: "\(localPath)Livery.xcframework") :
     Target.binaryTarget(name: "Livery",
                         url: "\(hostingUrl)Livery-\(latestVersion).zip",
-                        checksum: "911da712926fb834071b81319601f8f4b50af5925e52304a5de78606b35be1ac")
+                        checksum: "01a5f3aab9e0fca0014f6568da86e81b1e21389d5bd9f966cb2eb84893ec91f6")
 }
 
 let target = createTarget()

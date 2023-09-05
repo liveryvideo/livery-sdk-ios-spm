4430dc82c2f569e846c9eede608f809f0fcac256
CircleCI <distiller@static.38.39.184.41.cyberlynk.net>
2023-09-05 10:14:52 +0000

    Livery (2.1.0-beta.10)

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Livery",
-            url: "https://sdk-ios-binaries.liveryvideo.com/xcframework/Livery-2.1.0-beta.9.zip",
-            checksum: "d1364d7a9dd22292e14fb9747897ff88ef6b8afdfd0e8c6b91b4726a217f6793"
+            url: "https://sdk-ios-binaries.liveryvideo.com/xcframework/Livery-2.1.0-beta.10.zip",
+            checksum: "b5b54f5dcf471879dfd4b17ddd1803532ee4e37c0699dea59b092b78b0890156"
         ),
         .target(
             name: "LiverySDKWrapper",

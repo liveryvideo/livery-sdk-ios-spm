674b444d190431da8d2db4d398af02662ce66f20
CircleCI <ci@liveryvideo.com>
2025-02-18 14:42:54 +0000

    Livery (3.1.3-beta.01)

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
         .package(
             name: "AWSiOSSDKV2",
             url: "https://github.com/aws-amplify/aws-sdk-ios-spm",
-            from: "2.37.2"
+            from: "2.40.1"
         )
     ],
     targets: [
         .binaryTarget(
             name: "Livery",
-            url: "https://sdk-ios-binaries.liveryvideo.com/xcframework/Livery-3.1.2.zip",
-            checksum: "4233b636ba8a9276b6b9ff04ac302f3230c64efec0dee3916c25247bda3bc6fc"
+            url: "https://sdk-ios-binaries.liveryvideo.com/xcframework/Livery-3.1.3-beta.01.zip",
+            checksum: "ad19f4de4c4888b992e8e79700ff0b123018da13852178622e6554c59319f9de"
         ),
         .target(
             name: "LiverySDKWrapper",

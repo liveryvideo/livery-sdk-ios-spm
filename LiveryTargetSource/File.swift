f4ad83dd6002b6779b971930585089fc04f1bfda
Rui Rodrigues <rui.rodrigues@eyenov.com>
2025-11-20 14:22:24 +0000

    remove LiverySDKWrapper

--- LiveryTargetSource/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Jose Nogueira on 27/10/2021.
-//
-
-import Foundation
--- Package.swift
+++ Package.swift
@@ -7,17 +7,13 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
-        .library(name: "Livery", targets: ["LiverySDKWrapper"])
+        .library(name: "Livery", targets: ["Livery"])
     ],
     targets: [
         .binaryTarget(
             name: "Livery",
             url: "https://sdk-ios-binaries.livery.live/3.3.1-beta.2/Livery-spm-3.3.1-beta.2.zip",
             checksum: "bf3aa5a4e1899e7a2a80d3e749676d703b452ff8a6f8850c152bed8106894ad1"
-        ),
-        .target(
-            name: "LiverySDKWrapper",
-            path: "LiveryTargetSource"
         )
     ],
     swiftLanguageVersions: [.v5]

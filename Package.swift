df8c59fbec9910c8d1ef26d52866f4108da8fa54
CircleCI <ci@liveryvideo.com>
2025-11-20 11:44:17 +0000

    Livery (3.3.1-beta.2)

--- Package.swift
+++ Package.swift
@@ -9,26 +9,14 @@ let package = Package(
     products: [
         .library(name: "Livery", targets: ["LiverySDKWrapper"])
     ],
-    dependencies: [
-        .package(
-            name: "AWSiOSSDKV2",
-            url: "https://github.com/aws-amplify/aws-sdk-ios-spm",
-            from: "2.40.1"
-        )
-    ],
     targets: [
         .binaryTarget(
             name: "Livery",
-            url: "https://sdk-ios-binaries.livery.live/3.3.1-beta.1/Livery-spm-3.3.1-beta.1.zip",
-            checksum: "1641379627ea3d8406e4bb816031fb67c2847744f4f12ee01aba213a3bd523c2"
+            url: "https://sdk-ios-binaries.livery.live/3.3.1-beta.2/Livery-spm-3.3.1-beta.2.zip",
+            checksum: "bf3aa5a4e1899e7a2a80d3e749676d703b452ff8a6f8850c152bed8106894ad1"
         ),
         .target(
             name: "LiverySDKWrapper",
-            dependencies: [
-                "Livery",
-                .product(name: "AWSCore", package: "AWSiOSSDKV2"),
-                .product(name: "AWSPinpoint", package: "AWSiOSSDKV2")
-            ],
             path: "LiveryTargetSource"
         )
     ],

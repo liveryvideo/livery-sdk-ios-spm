59add5548e4a097e3431e7346e65393ed128fc09
CircleCI <ci@liveryvideo.com>
2025-11-25 14:22:34 +0000

    Livery (3.3.2)

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Livery",
-            url: "https://sdk-ios-binaries.livery.live/3.3.2-beta.3/Livery-spm-3.3.2-beta.3.zip",
-            checksum: "bc5819a9dc205e9444adfd150d9732a2d57a1ee50e853772cb83ffc1c644b158"
+            url: "https://sdk-ios-binaries.livery.live/3.3.2/Livery-spm-3.3.2.zip",
+            checksum: "dcdd7086a6260a108f14945df4aba4dcfa89a11bfb08bad1f148cbd91b8c6ddc"
         )
     ],
     swiftLanguageVersions: [.v5]
